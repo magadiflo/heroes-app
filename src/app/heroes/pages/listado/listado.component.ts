03b791a500e9d8f48f5398b78af8a8ca3dbbea35
Martín <magadiflo@gmail.com>
2022-01-03 11:22:32 -0500

    Interfaz héroe

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-listado',
@@ -10,11 +11,13 @@ import { HeroesService } from '../../services/heroes.service';
 })
 export class ListadoComponent implements OnInit {
 
+  heroes: Heroe[] = [];
+
   constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
     this.heroesService.getHeores()
-      .subscribe(h => console.log(h));
+      .subscribe(resp => this.heroes = resp);
   }
 
 
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { Observable } from 'rxjs';
+
+import { Heroe } from '../interfaces/heroes.interface';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -8,8 +12,8 @@ export class HeroesService {
 
   constructor(private http: HttpClient) { }
 
-  getHeores() {
-    return this.http.get('http://localhost:3000/heroes/');
+  getHeores(): Observable<Heroe[]> {
+    return this.http.get<Heroe[]>('http://localhost:3000/heroes/');
   }
 
 }
