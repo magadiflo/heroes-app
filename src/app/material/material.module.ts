cb41896d998a465dbb9f139d84ec013b58bd9daf
Martín <magadiflo@gmail.com>
2022-01-03 09:56:33 -0500

    Material Sidenav, Toolbar e íconos

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
   exports: [
-
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
   ]
 })
 export class MaterialModule { }
