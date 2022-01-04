0c9b1908660987b07badc978c7d6b80b23a981c2
Martín <magadiflo@gmail.com>
2022-01-04 10:20:43 -0500

    Tarea - PipeImagen

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -12,6 +12,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 
+import { ImagenPipe } from './pipes/imagen.pipe';
+
 
 
 @NgModule({
@@ -21,7 +23,8 @@ import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.
     HeroeComponent,
     HomeComponent,
     ListadoComponent,
-    HeroeTarjetaComponent
+    HeroeTarjetaComponent,
+    ImagenPipe,
   ],
   imports: [
     CommonModule,
