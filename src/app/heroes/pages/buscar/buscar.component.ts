5bd79acb022156ffdfc903a386585e836da9a4c4
Martín <magadiflo@gmail.com>
2022-01-05 10:03:32 -0500

    Autocomplete - segunda parte

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 import { Heroe } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
@@ -13,6 +14,7 @@ export class BuscarComponent implements OnInit {
 
   termino: string = '';
   heroes: Heroe[] = [];
+  heroeSeleccionado!: Heroe;
 
   constructor(private heroeService: HeroesService) { }
 
@@ -20,8 +22,15 @@ export class BuscarComponent implements OnInit {
   }
 
   buscando(): void {
-    this.heroeService.getHeores()
+    this.heroeService.getSugerencias(this.termino)
       .subscribe(heroes => this.heroes = heroes);
+  }
+
+  opcionSeleccionada(event: MatAutocompleteSelectedEvent) {
+    const heroe: Heroe = event.option.value;
+    this.termino = heroe.superhero;
+    this.heroeService.getHeroePorId(heroe.id!)
+      .subscribe(heroe => this.heroeSeleccionado = heroe);
   }
 
 }
