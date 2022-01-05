d993fef0e9bf040cc5d4f8ecc363c64192fcbf89
Martín <magadiflo@gmail.com>
2022-01-05 12:53:04 -0500

    Insertar en base de datos

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -22,9 +22,13 @@ export class HeroesService {
   getHeroePorId(id: string): Observable<Heroe> {
     return this.http.get<Heroe>(`${this.baseUrl}/heroes/${id}`);
   }
-  
-  getSugerencias(termino: string): Observable<Heroe[]>{
+
+  getSugerencias(termino: string): Observable<Heroe[]> {
     return this.http.get<Heroe[]>(`${this.baseUrl}/heroes?q=${termino}&_limit=6`);
+  }
+
+  agregarHeroe(heroe: Heroe): Observable<Heroe> {
+    return this.http.post<Heroe>(`${this.baseUrl}/heroes`, heroe);
   }
 
 }
