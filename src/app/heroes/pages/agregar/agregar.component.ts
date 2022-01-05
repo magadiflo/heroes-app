14357b06673526b5d7a0b5dcbf519162fac07877
Martín <magadiflo@gmail.com>
2022-01-05 12:18:36 -0500

    Diseño de la pantalla para agregar héroes

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
@@ -7,6 +9,26 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class AgregarComponent implements OnInit {
+
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ];
+
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: '',
+  }
 
   constructor() { }
 
