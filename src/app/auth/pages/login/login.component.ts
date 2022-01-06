022225ca42b716c6138740e04d239bad88b73341
Martín <magadiflo@gmail.com>
2022-01-06 11:59:51 -0500

    AuthService - Servicio para mantener el estado de la autenticación

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { AuthService } from '../../services/auth.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,10 +11,18 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private authService: AuthService) { }
 
   login(): void {
-    this.router.navigate(['/heroes']);
+    this.authService.login()
+      .subscribe(auth => {
+        console.log(auth);  
+        if(auth.id){
+          this.router.navigate(['/heroes']);
+        }
+      });
   }
 
 }
