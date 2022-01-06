6229cd202889676edaa30c8b5edf73deb444024f
Martín <magadiflo@gmail.com>
2022-01-06 11:41:56 -0500

    Pantalla de login básico

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { AuthRoutingModule } from './auth-routing.module';
 
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
+import { MaterialModule } from '../material/material.module';
 
 
 
@@ -16,6 +17,7 @@ import { RegistroComponent } from './pages/registro/registro.component';
   imports: [
     CommonModule,
     AuthRoutingModule,
+    MaterialModule,
   ]
 })
 export class AuthModule { }
