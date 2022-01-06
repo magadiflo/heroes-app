6c88ae97ac4f7bd07842be8d7a7db3db7c1ebc06
Martín <magadiflo@gmail.com>
2022-01-06 10:26:32 -0500

    Pipes puros e impuros

--- src/app/heroes/pipes/imagen.pipe.ts
+++ src/app/heroes/pipes/imagen.pipe.ts
@@ -3,11 +3,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Heroe } from '../interfaces/heroes.interface';
 
 @Pipe({
-  name: 'imagen'
+  name: 'imagen',
+  //pure: false, se volverá a ejecutar este pipe cada vez que el ciclo de detección de cambios de angular se dispare
 })
 export class ImagenPipe implements PipeTransform {
 
   transform(heroe: Heroe): string {
+    console.log('Pipe imagen se procesó...');  
     return (!heroe.id && !heroe.alt_img) ? `assets/no-image.png` : heroe.alt_img ? heroe.alt_img : `assets/heroes/${heroe.id}.jpg`;
   }
 
