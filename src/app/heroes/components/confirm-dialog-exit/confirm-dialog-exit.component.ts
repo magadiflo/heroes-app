bf4dd9b68d6d8c0341eea332ae2568a7c9646afe
Martín <magadiflo@gmail.com>
2024-05-26 23:13:26 -0500

    Agregando guard canDeactivate

---
+++ src/app/heroes/components/confirm-dialog-exit/confirm-dialog-exit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-dialog-exit',
+  templateUrl: './confirm-dialog-exit.component.html',
+  styles: [
+  ]
+})
+export class ConfirmDialogExitComponent {
+
+}
